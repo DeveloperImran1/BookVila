fd347465f9bbabeb893403e43d5d20db5d940691
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-11-07 12:09:04 +0600

    bannser section add

--- src/components/baner/Baner.jsx
+++ src/components/baner/Baner.jsx
@@ -10,10 +10,124 @@ import { Pagination } from 'swiper/modules';
 import { FaSearch } from "react-icons/fa";
 
 
+import { useEffect, useState } from "react";
+import { IoReorderThree } from "react-icons/io5";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+
+
 const Baner = () => {
+    const [selectCategory, setSelectCategory] = useState(null)
+
+
+    const boiShomogro = [
+        "মহাকালের কণ্ঠ",
+        "গোয়েন্দা কাহিনী সমগ্র",
+        "ভূতের গল্প",
+        "নক্ষত্রের রাত",
+        "জীবনের জলছবি",
+        "বাংলা সাহিত্যের ইতিহাস",
+        "ইতিহাসের অজানা অধ্যায়",
+        "গণিতের আনন্দ",
+        "পাখিদের নিয়ে গল্প",
+        "ভূগোলের বিস্ময়",
+        "শিশুতোষ গল্পসমগ্র",
+        "রহস্যময় পৃথিবী",
+        "প্রাণীর কাহিনী",
+        "বিজ্ঞানের বিস্ময়",
+        "বিশ্বের সেরা উপন্যাস",
+        "রবীন্দ্রনাথের কবিতা",
+        "প্রাচীন মিসরের ইতিহাস",
+        "যুগান্তরের কবিতা",
+        "অ্যালিস ইন ওয়ান্ডারল্যান্ড (বাংলা অনুবাদ)",
+        "বাংলা প্রবাদ প্রবচন",
+        "আধুনিক বাংলার কথা",
+        "চর্যাপদ ও প্রাচীন সাহিত্য",
+        "বাংলাদেশের মুক্তিযুদ্ধের গল্প",
+
+    ];
+
+    // বিষয়ের নামের অ্যারে
+    const subjects = [
+        "উপন্যাস",
+        "কবিতা",
+        "গল্প",
+        "ইতিহাস",
+        "বিজ্ঞান",
+        "দর্শন",
+        "ধর্ম",
+        "জীবনী",
+        "শিশুসাহিত্য",
+        "কৃষি",
+        "ভ্রমণকাহিনী",
+        "রাজনীতি",
+        "সমাজবিজ্ঞান",
+        "প্রযুক্তি",
+        "চিকিৎসাশাস্ত্র",
+        "গণিত",
+        "আইন",
+        "সাহিত্য",
+        "শিল্প ও সংস্কৃতি",
+        "ভৌগোলিক গবেষণা"
+    ];
+
+    // লেখকের নামের অ্যারে
+    const writers = [
+        "হুমায়ুন আহমেদ",
+        "জহির রায়হান",
+        "রবীন্দ্রনাথ ঠাকুর",
+        "কাজী নজরুল ইসলাম",
+        "আবুল বাশার",
+        "সৈয়দ মুজতবা আলী",
+        "সুনীল গঙ্গোপাধ্যায়",
+        "আবুল মনসুর আহমদ",
+        "বিভূতিভূষণ বন্দ্যোপাধ্যায়",
+        "মুহম্মদ জাফর ইকবাল",
+        "শীর্ষেন্দু মুখোপাধ্যায়",
+        "আনিসুল হক",
+        "আহমদ ছফা",
+        "অনীশ দাস অপু",
+        "বেগম রোকেয়া",
+        "ফয়েজ আহমেদ",
+        "মীর মশাররফ হোসেন",
+        "তসলিমা নাসরিন",
+        "জাকির তালুকদার",
+        "প্রেমচাঁদ"
+    ];
+
+    // প্রকাশনীর নামের অ্যারে
+    const publications = [
+        "প্রথমা প্রকাশন",
+        "অন্যপ্রকাশ",
+        "আনন্দ পাবলিশার্স",
+        "বাংলা একাডেমি",
+        "বিদ্যাপ্রকাশ",
+        "ইত্যাদি গ্রন্থ প্রকাশ",
+        "কাকলী প্রকাশনী",
+        "পাঠক সমাবেশ",
+        "মাওলা ব্রাদার্স",
+        "সাহিত্য প্রকাশ",
+        "অনন্যা প্রকাশনী",
+        "অন্তর্জলী প্রকাশন",
+        "বঙ্গপ্রকাশ",
+        "সৃজনশীল প্রকাশনী",
+        "সম্প্রতি প্রকাশনী",
+        "জ্ঞানভবন প্রকাশনী",
+        "বিশ্বসাহিত্য কেন্দ্র",
+        "শিখা প্রকাশনী",
+        "বর্ণমালা প্রকাশনী",
+        "সাহিত্য সংসদ"
+    ];
+
+
+
+
     return (
+
         <>
-            <section className="hidden md:flex items-center justify-between bg-[#00bffe] p-2 rounded-md">
+            <section className="hidden md:flex container mt-3 items-center justify-between bg-[#00bffe] p-2 rounded-md">
                 <span className="relative   ">
                     <input type="text" placeholder="Search Anything" className="  bg-white  md:w-[200px] lg:w-[300px]   px-2 border-none focus:outline-none focus:border-none bg-transparent rounded-full md:rounded-l-full " />
                     <FaSearch size={22} className='text-gray-500 absolute right-3 top-[20%]'></FaSearch>
@@ -56,16 +170,55 @@ const Baner = () => {
 
 
 
-            <div className='flex  gap-[10px] '>
+            <div className='flex relative gap-[10px] container p-0'>
 
-                <div className="md:w-[30%] lg:w-[20%] hidden md:flex bg-[url('https://i.ibb.co/JcgWmN3/banner1-png.png')] bg-cover  pl-[50px] pt-[50px] pb-[50px] rounded-md  justify-between items-start">
-                    <div>
-                        <h1 className="text-black font-bold text-[24px] w-[150px] ">redmi note 12
-                            Pro+ 5g</h1>
-                        <h1 className="text-[#7a7b7c] font-semibold  ">Rise to the challenge</h1>
+                <div className="md:w-[30%] lg:w-[20%] hidden md:flex  bg-white pt-[10px] pb-[50px] rounded-md  justify-between items-start  border-2">
+                    <ul onMouseLeave={() => setSelectCategory(null)} className="w-full ">
+                        <Link onMouseOver={() => setSelectCategory("books")} className="py-3 px-3 font-semibold w-full hover:bg-[#00bffe] hover:text-white rounded-md flex justify-between items-center">
+                            <p>Books</p>
+                            <MdOutlineArrowForwardIos></MdOutlineArrowForwardIos>
+                        </Link>
+                        <Link onMouseOver={() => setSelectCategory("subjects")} className="py-3 px-3 font-semibold w-full hover:bg-[#00bffe] hover:text-white rounded-md flex justify-between items-center">
+                            <p>Subjects</p>
+                            <MdOutlineArrowForwardIos></MdOutlineArrowForwardIos>
+                        </Link>
+                        <Link onMouseOver={() => setSelectCategory("writers")} className="py-3 px-3 font-semibold w-full hover:bg-[#00bffe] hover:text-white rounded-md flex justify-between items-center">
+                            <p>Writer</p>
+                            <MdOutlineArrowForwardIos></MdOutlineArrowForwardIos>
+                        </Link>
+                        <Link onMouseOver={() => setSelectCategory("publications")} className="py-3 px-3 font-semibold w-full hover:bg-[#00bffe] hover:text-white rounded-md flex justify-between items-center">
+                            <p>Publications</p>
+                            <MdOutlineArrowForwardIos></MdOutlineArrowForwardIos>
+                        </Link>
 
-                    </div>
+                        {
+                            selectCategory && <div className="bg-white absolute right-0 top-0 z-50 w-full md:w-[70%] lg:w-[80%]">
+                                <ul className="w-full px-6 py-4 flex flex-col flex-wrap h-[430px] ">
+                                    {
+                                        selectCategory === "books" ?
+                                            boiShomogro?.map((book, index) => <Link key={index} className="py-3 px-3 font-normal w-[250px] hover:text-[#00bffe] hover:underline">
+                                                {book}
+                                            </Link>) :
+                                            selectCategory === "subjects" ? subjects?.map((book, index) => <Link key={index} className="py-3 px-3 font-normal w-[250px] hover:text-[#00bffe] hover:underline">
+                                                {book}
+                                            </Link>) :
+                                                selectCategory === "writers" ? writers?.map((book, index) => <Link key={index} className="py-3 px-3 font-normal w-[250px] hover:text-[#00bffe] hover:underline">
+                                                    {book}
+                                                </Link>) :
+                                                    selectCategory === "publications" ? publications?.map((book, index) => <Link key={index} className="py-3 px-3 font-normal w-[250px] hover:text-[#00bffe] hover:underline">
+                                                        {book}
+                                                    </Link>) : ""
+                                    }
+
+                                </ul>
+                            </div>
+                        }
+
+
+
+                    </ul>
                 </div>
+
 
                 <Swiper pagination={true} modules={[Pagination]} className="mySwiper w-full md:w-[70%] lg:w-[80%] ">
                     <SwiperSlide className="bg-[url('https://i.ibb.co/GVN2nLN/slider3-png.png')] bg-center bg-cover pl-[80px] pt-[80px] pb-[80px] rounded-md  ">
@@ -219,6 +372,11 @@ const Baner = () => {
 
                 </Swiper>
             </div>
+
+
+
+
+
 
         </>
     );
